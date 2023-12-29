f970b795f4c098610255940dd1c18649e4971e8f
Ankit choube <91187951+ankit12345677@users.noreply.github.com>
2023-12-30 04:27:37 +0530

    Update index.js

--- NodeProject1/index.js
+++ NodeProject1/index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { Console } = require("console");
 
 const app = express();
-const PORT = 5000 || process.env.PORT
+const PORT = process.env.PORT
 
 const users = [{}]
 
